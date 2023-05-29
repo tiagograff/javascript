75988f51546c546ac05f94e4e2f8f97600c2a65b
tiago graffunder <tiagopinzgraffunder@gmail.com>
2023-05-29 15:24:05 -0300

    day of code

--- 7DaysOfCode/day05.js
+++ 7DaysOfCode/day05.js
@@ -6,50 +6,39 @@ let outros = []
 let ok = true
 
 while (ok === true){
-    const escolha = prompt('deseja adicionar uma comida na sua lista de compras? "sim" ou "não"')
+    let escolha = prompt('deseja adicionar uma comida na sua lista de compras? "sim" ou "não"')
 if (escolha.toLowerCase() === 'sim'){
     let comida = prompt('qual comida você deseja inserir?')
-    let numero = prompt(`onde você deseja inserir ${comida}
+    let numero = Number(prompt(`onde você deseja inserir ${comida}
     1. frutas
     2. laticineos
     3. congelados
     4. doces
-    5. outros`)
-    if (numero === '1'){
+    5. outros`))
+    if (numero === 1){
         frutas.push(comida)
-    }else if (numero === '2'){
-        frutas.push(comida)
-    }else if (numero === '2'){
-        frutas.push(comida)
-    }else if (numero === '3'){
-        frutas.push(comida)
-    }else if (numero === '4'){
-        frutas.push(comida)
-    }else if (numero === '5'){
-        frutas.push(comida)
+    }else if (numero === 2){
+        laticineos.push(comida)
+    }else if (numero === 3){
+        congelados.push(comida)
+    }else if (numero === 4){
+        doces.push(comida)
+    }else if (numero === 5){
+        outros.push(comida)
     }else{
         alert('valor inválido, tente novamente')
     }
-    }else if (escolha.toLowerCase() === 'não'){
-        alert('ok...')
-        ok = false
-    }else{
-        alert('valor inválido')
-    }
+}else if (escolha.toLowerCase() === 'não'){
+    alert('ok...')
+    ok = false
+}else{
+    alert('valor inválido')
+}
 }
 
-let i = 0
-
-const mostraLista = (array) => {
-    array.forEach(element => {
-        alert(`lista ${i}:
-        ${element}`)
-        i++
-    })
-}
-
-mostraLista(frutas)
-mostraLista(laticineos)
-mostraLista(congelados)
-mostraLista(doces)
-mostraLista(outros)
+alert(`
+lista 1: ${frutas}
+lista 2: ${laticineos}
+lista 3: ${congelados}
+lista 4: ${doces}
+lista 5: ${outros}`)
